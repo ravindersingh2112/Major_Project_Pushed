b24208c80481352c3f519c3ccd89ed1446648b0d
ravindersingh2112 <201b209@juetguna.in>
2023-12-01 14:19:58 +0530

    Update eda_app.py
    final edit

--- eda_app.py
+++ eda_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 def run_eda_app():
 	st.subheader("Real Estate : Data Analysis")
 
-	submenu = st.sidebar.selectbox("Submenu", ["Descriptive", "Plots"])
+	submenu = st.sidebar.selectbox("Submenu", ["Descriptive"])
 	df = pd.read_csv("Final_Project.csv")
 
 	if submenu == "Descriptive":
@@ -23,37 +23,3 @@ def run_eda_app():
 
 		with st.expander("Location Distribution"):
 			st.dataframe(df["Region"].value_counts().head(30))
-
-	elif submenu == "Plots":
-
-		with st.expander("Price Range Distribution"):
-			img2 = Image.open("Price_Range_Distribution.png")
-			st.image(img2)
-
-		with st.expander("Price with respect to Floor"):
-			img3 = Image.open("Property_Floor_Numbers_Bar.png")
-			st.image(img3)
-		
-		with st.expander("Price with respect to Bedroom and Bathroom"):
-			img4 = Image.open("BednBath_Price_Bar.png")
-			st.image(img4)
-
-		with st.expander("Price with respect to Property Age"):
-			img5 = Image.open("Price_Age_Distribution.png")
-			st.image(img5)
-
-		with st.expander("Price with respect to SqFt Area"):
-			img6 = Image.open("SqFt_Area_Price_Scatter.png")
-			st.image(img6)
-
-		with st.expander("Central Mumbai Property Price"):
-			img7 = Image.open("Central Mumbai.png")
-			st.image(img7)
-
-		with st.expander("South Mumbai Property Price"):
-			img8 = Image.open("South Mumbai.png")
-			st.image(img8)
-
-		with st.expander("Thane Property Price"):
-			img9 = Image.open("Thane.png")
-			st.image(img9)
